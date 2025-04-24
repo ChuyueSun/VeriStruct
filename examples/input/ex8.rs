ba7afc82ee888d0f688d3239c0b1a65ea32fa04b
Chenyuan Yang <cy1yang@outlook.com>
2025-04-24 15:15:41 -0500

    [delete] delete the original examples and lemmas

--- examples/input/ex8.rs
+++
@@ -1,7 +0,0 @@
-impl<T: Copy> View for RingBuffer<T> {
-    type V = Seq<T>;
-
-    closed spec fn view(&self) -> Self::V {
-        // TODO: implement this. Use sequence to represent the ring buffer.
-    }
-}
