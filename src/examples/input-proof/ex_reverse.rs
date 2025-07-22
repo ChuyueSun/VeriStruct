ce2cbe9f0a6d8f96afaf73bafebae6b5faf65cea
Chuyue Sun <chuyue99@gmail.com>
2025-07-22 15:18:50 -0500

    fix few shot examples

---
+++ src/examples/input-proof/ex_reverse.rs
@@ -0,0 +1,16 @@
+fn reverse(v: &mut Vec<u64>)
+    ensures
+        v.len() == old(v).len(),
+        forall|i: int| 0 <= i < old(v).len() ==> v[i] == old(v)[old(v).len() - i - 1],
+{
+    let length = v.len();
+    let ghost v1 = v@;
+    for n in 0..(length / 2)
+        // TODO: add invariant
+    {
+        let x = v[n];
+        let y = v[length - 1 - n];
+        v.set(n, y);
+        v.set(length - 1 - n, x);
+    }
+}
